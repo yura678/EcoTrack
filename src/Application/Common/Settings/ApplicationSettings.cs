3f6f9f964c3a19664702db1dbc6a64d4a970c088
yura76 <yriyvorobey@gmail.com>
2026-02-26 16:15:46 +0200

    Initial commit: Project structure setup

---
+++ src/Application/Common/Settings/ApplicationSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Common.Settings;
+
+public class ApplicationSettings
+{
+    public ConectionStrings? ConnectionStrings { get; set; } 
+}
+
+public class ConectionStrings
+{
+    public string? DefaultConnection { get; set; }
+}
